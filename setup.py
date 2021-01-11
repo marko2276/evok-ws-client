7027b8033fc4a673caae951b4056ec3cecaf707c
Marko Vaupotic <marko.vaupotic@aviatnet.com>
2021-01-11 22:39:40 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+        name='evok-ws-client',
+        version='0.0.1',
+        description='Client for connectin to Unipi devices runing EVOK via websockets',
+        author="Marko Vaupotic",
+        url='https://github.com/marko2276/evok-ws-client',
+        license='MIT',
+        packages=setuptools.find_packages(),
+        python_requires='>=3',
+)
